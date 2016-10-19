d53f36b61813cad09363e8fae9b4938fa36ff5e5
WAHHAB BALDWIN <wahhabb@gmail.com>
2016-10-19 09:44:36 -0600

    Don't show variants

---
+++ series_dump.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+import MySQLdb
+import re
+
+host = "localhost"
+passwd = "rtsunl!mited"
+user = "tim"
+dbname = "rts_db"
+
+db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=dbname)
+cursor = db.cursor()
+
+sql = "SELECT s.id, s.name, sort_name, publisher_id, p.name, s.year_began FROM gcd_series s " \
+      "INNER JOIN gcd_publisher p ON s.publisher_id = p.id WHERE s.country_id = 225 ORDER BY s.name;"
+
+cursor.execute(sql)
+# 0 = id, 1 = name, 2 = sort_name, 3 = publisher_id, 4 = pub. name, 5 = year_began
+count = 0
+row = cursor.fetchone()
+while row is not None:
+    row = cursor.fetchone()
+    print(','.join(map(str, row)))
+    count += 1
+    # if count > 4:
+    #     break
+    row = cursor.fetchone()
+db.close()
+
--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print('yes')
-class Parent:
+def fix_none(field):
+    if field == None:
+        return ''
+    return field
+
+print(fix_none('abc'))
+print(fix_none(None) + 'def')
+
+/Users/wahhab/Sites/rts/env/lib/python3.5/site-packages/django/db/models/fields/__init__.py:1430:
+RuntimeWarning: DateTimeField Issue.added_date received a naive datetime (2016-10-17 18:33:25.369050) while time zone support is active.
+  RuntimeWarning)
